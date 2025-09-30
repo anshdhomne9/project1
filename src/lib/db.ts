e94984a1c54701cdfe0134655bc21347db264395
Gurvesh Dhomne <anshdhomne7@gmail.com>
2025-09-30 16:54:00 +0530

    fix: Add type declaration for global mongoose

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,13 @@
 
 import mongoose from 'mongoose';
 
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
